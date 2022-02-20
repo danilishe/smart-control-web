a049f535cea22bc42475448fe1957ea1f55d61d9
danilishe <suetin.danil@gmail.com>
2022-02-20 14:40:46 +0400

    fix href

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export const NavBar: React.FC = () => (<nav>
         <div className="nav-wrapper red darken-3">
-            <a href="/" className="brand-logo center">SmartControl Web</a>
+            <a href="." className="brand-logo center">SmartControl Web</a>
             <ul className="left hide-on-med-and-down">
-                <li><a href="/">Загрузить</a></li>
-                <li><a href="/">Сохранить</a></li>
-                <li><a href="/">Экспорт</a></li>
+                <li><a href=".">Загрузить</a></li>
+                <li><a href=".">Сохранить</a></li>
+                <li><a href=".">Экспорт</a></li>
             </ul>
             <ul className="right hide-on-med-and-down">
-                <li><a href="/"><i className="material-icons left">settings</i>Настройки</a></li>
+                <li><a href="."><i className="material-icons left">settings</i>Настройки</a></li>
             </ul>
         </div>
     </nav>
