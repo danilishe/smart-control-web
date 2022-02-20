5013a627d10ad88fe98aad39dedc5c9fb3aba87e
danilishe <suetin.danil@gmail.com>
2022-02-20 13:49:32 +0400

    add material theme add nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const NavBar: React.FunctionComponent = () => (<nav>
+        <div className="nav-wrapper">
+            <a href="#" className="brand-logo center">SmartControl Web</a>
+            <ul className="left hide-on-med-and-down">
+                <li><a href="#">Загрузить</a></li>
+                <li><a href="#">Сохранить</a></li>
+                <li><a href="#">Экспорт</a></li>
+            </ul>
+            <ul className="right hide-on-med-and-down">
+                <li><a href="#"><i className="material-icons left">settings</i>Настройки</a></li>
+            </ul>
+        </div>
+    </nav>
+)
