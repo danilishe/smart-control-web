035415726a8d0f8ba909a713dd12e6d39b3dad3f
danilishe <suetin.danil@gmail.com>
2022-02-20 14:12:09 +0400

    remove warnings

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {NavBar} from "./components/NavBar";
 function App() {
     return <>
         <NavBar/>
+        <div className="container">
+
+        </div>
     </>
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-export const NavBar: React.FunctionComponent = () => (<nav>
-        <div className="nav-wrapper">
-            <a href="#" className="brand-logo center">SmartControl Web</a>
+export const NavBar: React.FC = () => (<nav>
+        <div className="nav-wrapper red darken-3">
+            <a href="/" className="brand-logo center">SmartControl Web</a>
             <ul className="left hide-on-med-and-down">
-                <li><a href="#">Загрузить</a></li>
-                <li><a href="#">Сохранить</a></li>
-                <li><a href="#">Экспорт</a></li>
+                <li><a href="/">Загрузить</a></li>
+                <li><a href="/">Сохранить</a></li>
+                <li><a href="/">Экспорт</a></li>
             </ul>
             <ul className="right hide-on-med-and-down">
-                <li><a href="#"><i className="material-icons left">settings</i>Настройки</a></li>
+                <li><a href="/"><i className="material-icons left">settings</i>Настройки</a></li>
             </ul>
         </div>
     </nav>
