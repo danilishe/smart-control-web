e437df1918de4c993ef4da54b7b120c02e5f0183
danilishe <suetin.danil@gmail.com>
2022-05-03 20:37:18 +0400

    move buttons to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { ControlPanel } from './components/ControlPanel';
 import { EffectsCatalogue } from "./components/EffectsCatalogue";
 import { NavBar } from "./components/NavBar";
 import { ProgramList } from "./components/ProgramList";
@@ -10,14 +9,12 @@ function App() {
     return <>
         <NavBar />
         <div className="container">
-
             <div className="row  mt-3">
                 <div className="col-3">
                     <EffectsCatalogue />
                 </div>
                 <div className="col g-2">
                     {/* <PreviewArea /> */}
-                    <ControlPanel />
                     <ProgramList />
                 </div>
             </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { MouseEventHandler, useRef } from "react";
+import Parameters from "../parameters";
 
-export const NavBar: React.FC = () => (<nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"/>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
+export const NavBar = () => {
+    const downloadLinkRef = useRef<HTMLAnchorElement>(null)
+    const exportProgram: MouseEventHandler<HTMLAnchorElement> = (event) => {
+        event.preventDefault();
+        const file = new Blob([ 'jj90099093dasdfswe323425gtwdr453' ], {
+            type: "application/octet-stream",
+        })
+        const reader = new FileReader();
+        reader.onload = () => {
+            downloadLinkRef.current!.href = reader.result as string;
+            downloadLinkRef.current!.click();
+        }
+        reader.readAsDataURL(file);
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" target="_blank" href="https://is-led.ru">Smart Control Web</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link disabled" aria-current="page" href="\">Настройки</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="\">Сохранить</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" onClick={exportProgram}>Экспорт</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>)
+            <a ref={downloadLinkRef} href=""
+               type="application/octet-stream"
+               download={Parameters.defaultExportFileName}
+               target="_self" hidden />
+        </nav>);
+}
